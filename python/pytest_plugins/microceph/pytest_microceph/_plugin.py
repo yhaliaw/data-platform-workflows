ce4e809f5e8d981e560e06f3ae7850d9c4e542a2
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-01-16 14:42:15 +0100

    fix key values

--- python/pytest_plugins/microceph/pytest_microceph/_plugin.py
+++ python/pytest_plugins/microceph/pytest_microceph/_plugin.py
@@ -1,13 +1,10 @@
 import dataclasses
-import json
 import logging
 import os
 import subprocess
 
 import boto3
 import pytest
-import random
-import string
 
 
 @dataclasses.dataclass(frozen=True)
@@ -15,33 +12,6 @@ class ConnectionInformation:
     access_key_id: str
     secret_access_key: str
     bucket: str
-
-
-def _gen_random_string(length):
-    return ''.join(
-        random.choice(
-            string.ascii_uppercase + string.ascii_lowercase + string.digits
-        ) for _ in range(10)
-    )
-
-
-def _create_new_keys(access_key, secret_key):
-    logger.info("Creating keys")
-    subprocess.run(
-        [
-            "sudo",
-            "microceph.radosgw-admin",
-            "key",
-            "create",
-            "--uid=test",
-            "--key-type=s3",
-            "--access-key", access_key,
-            "--secret-key", secret_key,
-        ],
-        capture_output=True,
-        check=True,
-        encoding="utf-8",
-    ).stdout
 
 
 @pytest.fixture(scope="session")
@@ -52,10 +22,7 @@ def microceph():
         ["snap", "list"]
     ).decode("utf-8"):
         logger.info("Microceph already installed, keeping it")
-        access_key = _gen_random_string(10)
-        secret_key = _gen_random_string(24)
-        _create_new_keys(access_key, secret_key)
-        return ConnectionInformation(access_key, secret_key, _BUCKET)
+        return ConnectionInformation(_ACCESS_KEY, _SECRET_KEY, _BUCKET)
 
     logger.info("Setting up microceph")
     subprocess.run(["sudo", "snap", "install", "microceph"], check=True)
@@ -65,7 +32,7 @@ def microceph():
         check=True
     )
     subprocess.run(["sudo", "microceph", "enable", "rgw"], check=True)
-    output = subprocess.run(
+    subprocess.run(
         [
             "sudo",
             "microceph.radosgw-admin",
@@ -80,19 +47,34 @@ def microceph():
         check=True,
         encoding="utf-8",
     ).stdout
-    key = json.loads(output)["keys"][0]
-    key_id = key["access_key"]
-    secret_key = key["secret_key"]
+    logger.info("Creating keys")
+    subprocess.run(
+        [
+            "sudo",
+            "microceph.radosgw-admin",
+            "key",
+            "create",
+            "--uid=test",
+            "--key-type=s3",
+            "--access-key", _ACCESS_KEY,
+            "--secret-key", _SECRET_KEY,
+        ],
+        capture_output=True,
+        check=True,
+        encoding="utf-8",
+    ).stdout
     logger.info("Creating microceph bucket")
     boto3.client(
         "s3",
         endpoint_url="http://localhost",
-        aws_access_key_id=key_id,
-        aws_secret_access_key=secret_key,
+        aws_access_key_id=_ACCESS_KEY,
+        aws_secret_access_key=_SECRET_KEY,
     ).create_bucket(Bucket=_BUCKET)
     logger.info("Set up microceph")
-    return ConnectionInformation(key_id, secret_key, _BUCKET)
+    return ConnectionInformation(_ACCESS_KEY, _SECRET_KEY, _BUCKET)
 
 
+_ACCESS_KEY = "access-key"
+_SECRET_KEY = "secret-key"
 _BUCKET = "testbucket"
 logger = logging.getLogger(__name__)
